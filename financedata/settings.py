1c1624fddd849dc8f47c4838ee272126d102933a
Neeky <1721900707@qq.com>
2017-04-23 13:32:55 +0800

    add moneny supply table

--- financedata/settings.py
+++ financedata/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'monetaryAuthority.apps.MonetaryauthorityConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'financedata.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fdb',
+        'HOST':'127.0.0.1',
+        'USER':'appuser',
+        'PASSWORD':'Pass@123',
+        'PORT':'3306'
     }
 }
 
--- monetaryAuthority/models.py
+++ monetaryAuthority/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# 针对中国人民银行的统计数据进行建模
+
+class MoneySupply(models.Model):
+    """货币供应量"""
+    yearMonth=models.DateField()
+    m0=models.FloatField()
+    m1=models.FloatField()
+    m2=models.FloatField()
+
+

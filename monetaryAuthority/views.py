050acea27eb35bfc04d4334d87f6e7bf800ad69c
Neeky <1721900707@qq.com>
2017-04-24 14:41:09 +0800

    add monetary authority

--- monetaryAuthority/views.py
+++ monetaryAuthority/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    html=("<html>"
+    "<head></head>"
+    "<body>this is ma.views.index</body>"
+    "</html>")
+    return HttpResponse(html)
+
